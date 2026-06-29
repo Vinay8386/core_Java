2a75119bb0b3c11c44eaf30e47c143e1feed6018
Vinay <v0k9m5@gmail.com>
2026-06-29 18:10:51 +0530

    Java8 code practice

---
+++ Java8_code/PracticeSet_2/StrinClass.java
@@ -0,0 +1,7 @@
+package Java8_code.PracticeSet_2;
+
+public class StrinClass {
+    public static void main(String[] args) {
+        String str = "Java is Powerful and Java is Fast and Secure";
+    }
+}
---
+++ Java8_code/PracticeSet_3/IntegerClass.java
@@ -0,0 +1,10 @@
+package Java8_code.PracticeSet_3;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IntegerClass {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(12, 5, 8, 12, 19, 25, 8, 30, 45, 50, 67, 89, 90, 100, 23, 17, 50, 60, 75, 99);
+    }
+}
